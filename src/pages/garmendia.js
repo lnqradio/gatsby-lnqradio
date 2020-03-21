dd4bf309a7ce086d9ac26e65714c909c1a3c0fb3
santuan <smrcouto88@gmail.com>
2020-03-21 17:18:41 -0300

    0.1.3

--- src/pages/garmendia.js
+++ src/pages/garmendia.js
@@ -8,15 +8,10 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="Laboratorios Garmendia" />
     <div className="max-w-6xl pt-6  md:pt-12 m-auto text-left relative z-50">
-      <h1 className="text-white text-3xl font-thin animated fadeInUp slow text-center px-6">
-        <span className="animated block flash infinite slower text-red-500  pt-2 text-xl uppercase mb-3 font-mono tracking-widest">
-          Ensayo sobre las formas de ver
-        </span>
-      </h1>
       <div className="flex flex-col justify-center mt-6 max-w-2xl m-auto">
         <iframe
           src="https://mixlr.com/users/7717035/embed?color=0d0314"
-          width="420px"
+          width="100%"
           className="max-w-sm m-auto my-6"
           height="150px"
           scrolling="no"
@@ -24,6 +19,11 @@ const NotFoundPage = () => (
           marginheight="0"
           marginwidth="0"
         ></iframe>
+        <h1 className="text-white text-xl font-thin animated fadeInUp slow text-center pt-6">
+          <span className="animated block flash infinite slower text-red-500  pt-2 text-xl uppercase mb-3 font-mono tracking-widest">
+            Ensayo sobre formas de ver
+          </span>
+        </h1>
         <iframe
           src="https://player.twitch.tv/?autoplay=false&video=v571546383"
           frameborder="0"
@@ -36,7 +36,7 @@ const NotFoundPage = () => (
       </div>
       <article className="changelog">
         <h1 className="text-white text-xl py-6  italic">
-          # Registro de cambios. v.0.1.2
+          # Registro de cambios. v.0.1.3
         </h1>
         <p>
           + Se agregaron mas de 3 items por categoria en la home. <br />
