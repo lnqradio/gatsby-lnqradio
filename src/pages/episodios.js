9e45c340d5f6f026029779ea5b5e2533a7904127
santuan <smrcouto88@gmail.com>
2021-04-04 20:06:41 -0300

    el doble

--- src/pages/episodios.js
+++ src/pages/episodios.js
@@ -79,65 +79,92 @@ const EpisodiosPage = () => {
         />
       </div>
       <div
-          className="relative block w-full max-w-2xl mx-auto my-6 font-mono shadow-sm"
+        className="relative block w-full max-w-2xl mx-auto my-6 font-mono shadow-sm"
+        style={{
+          background: "rgba(40, 17, 54, 0.91)",
+        }}
+      >
+        <div
+          className="flex items-center justify-between listen"
           style={{
-            background: "rgba(40, 17, 54, 0.91)",
+            opacity: 0.9,
           }}
         >
-          <div
-            className="flex items-center justify-between listen"
-            style={{
-              opacity: 0.9,
-            }}
-          >
-            <iframe
-              width="100%"
-              height="350"
-              scrolling="no"
-              title="La deuda"
-              frameBorder="no"
-              allow="autoplay"
-              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1016611732&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
-            ></iframe>
-          </div>
-
-          <div className="relative flex flex-col items-center justify-center px-4 pb-1">
-            <h2 className="flex items-baseline w-full pt-2 text-5xl font-bold leading-normal text-left text-white uppercase">
-              <span className="flex-1">El Engaño</span>
-            </h2>
-          </div>
-
+          <iframe
+            width="100%"
+            height="350"
+            scrolling="no"
+            title="La deuda"
+            frameBorder="no"
+            allow="autoplay"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1022292985&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
+          ></iframe>
         </div>
+
+        <div className="relative flex flex-col items-center justify-center px-4 pb-1">
+          <h2 className="flex items-baseline w-full pt-2 text-5xl font-bold leading-normal text-left text-white uppercase">
+            <span className="flex-1">El Doble, Diario Intimo Y Ficción.</span>
+          </h2>
+        </div>
+      </div>
       <div
-          className="relative block w-full max-w-2xl mx-auto my-6 font-mono shadow-sm"
+        className="relative block w-full max-w-2xl mx-auto my-6 font-mono shadow-sm"
+        style={{
+          background: "rgba(40, 17, 54, 0.91)",
+        }}
+      >
+        <div
+          className="flex items-center justify-between listen"
           style={{
-            background: "rgba(40, 17, 54, 0.91)",
+            opacity: 0.9,
           }}
         >
-          <div
-            className="flex items-center justify-between listen"
-            style={{
-              opacity: 0.9,
-            }}
-          >
-            <iframe
-              width="100%"
-              height="350"
-              scrolling="no"
-              title="La deuda"
-              frameBorder="no"
-              allow="autoplay"
-              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1010060833&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
-            ></iframe>
-          </div>
-
-          <div className="relative flex flex-col items-center justify-center px-4 pb-1">
-            <h2 className="flex items-baseline w-full pt-2 text-5xl font-bold leading-normal text-left text-white uppercase">
-              <span className="flex-1">La nueva normalidad</span>
-            </h2>
-          </div>
-
+          <iframe
+            width="100%"
+            height="350"
+            scrolling="no"
+            title="La deuda"
+            frameBorder="no"
+            allow="autoplay"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1016611732&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
+          ></iframe>
         </div>
+
+        <div className="relative flex flex-col items-center justify-center px-4 pb-1">
+          <h2 className="flex items-baseline w-full pt-2 text-5xl font-bold leading-normal text-left text-white uppercase">
+            <span className="flex-1">El Engaño</span>
+          </h2>
+        </div>
+      </div>
+      <div
+        className="relative block w-full max-w-2xl mx-auto my-6 font-mono shadow-sm"
+        style={{
+          background: "rgba(40, 17, 54, 0.91)",
+        }}
+      >
+        <div
+          className="flex items-center justify-between listen"
+          style={{
+            opacity: 0.9,
+          }}
+        >
+          <iframe
+            width="100%"
+            height="350"
+            scrolling="no"
+            title="La deuda"
+            frameBorder="no"
+            allow="autoplay"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1010060833&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
+          ></iframe>
+        </div>
+
+        <div className="relative flex flex-col items-center justify-center px-4 pb-1">
+          <h2 className="flex items-baseline w-full pt-2 text-5xl font-bold leading-normal text-left text-white uppercase">
+            <span className="flex-1">La nueva normalidad</span>
+          </h2>
+        </div>
+      </div>
       <div>
         <div
           className="relative block w-full max-w-2xl mx-auto my-6 font-mono shadow-sm"
--- src/pages/index.js
+++ src/pages/index.js
@@ -140,13 +140,13 @@ const IndexPage = () => {
               title="La deuda"
               frameBorder="no"
               allow="autoplay"
-              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1016611732&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1022292985&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
             ></iframe>
           </div>
 
           <div className="relative flex flex-col items-center justify-center px-4 pb-1">
             <h2 className="flex items-baseline w-full pt-2 text-5xl font-bold leading-normal text-left text-white uppercase">
-              <span className="flex-1">El Engaño</span>
+              <span className="flex-1">El Doble, Diario Intimo Y Ficción.</span>
             </h2>
           </div>
 
