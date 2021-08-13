110e6f57ca34bed8f3adc9f5b41034ff0a8b8003
stn <smrcouto88@gmail.com>
2021-08-13 10:54:09 -0300

    Episodios que restaban subir

--- src/pages/episodios.js
+++ src/pages/episodios.js
@@ -79,7 +79,65 @@ const EpisodiosPage = () => {
         />
       </div>
       <div className="grid max-w-6xl grid-cols-1 gap-6 mx-auto md:grid-cols-2">
-      <div
+        <div
+          className="relative block w-full max-w-2xl mx-auto font-mono shadow-sm"
+          style={{
+            background: "rgba(40, 17, 54, 0.91)",
+          }}
+        >
+          <div
+            className="flex items-center justify-between listen"
+            style={{
+              opacity: 0.9,
+            }}
+          >
+            <iframe
+              width="100%"
+              height="350"
+              scrolling="no"
+              title="La deuda"
+              frameBorder="no"
+              allow="autoplay"
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1105144891&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
+            ></iframe>
+          </div>
+          <div className="relative flex flex-col items-center justify-center px-4 pb-1">
+            <h2 className="flex items-baseline w-full pt-2 text-3xl font-bold leading-normal text-center text-white uppercase">
+              <span className="flex-1">
+                Historias De Gente Común (como Vos, Como Yo, Como Aquel)
+              </span>
+            </h2>
+          </div>
+        </div>
+        <div
+          className="relative block w-full max-w-2xl mx-auto font-mono shadow-sm"
+          style={{
+            background: "rgba(40, 17, 54, 0.91)",
+          }}
+        >
+          <div
+            className="flex items-center justify-between listen"
+            style={{
+              opacity: 0.9,
+            }}
+          >
+            <iframe
+              width="100%"
+              height="350"
+              scrolling="no"
+              title="La deuda"
+              frameBorder="no"
+              allow="autoplay"
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1101133099&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
+            ></iframe>
+          </div>
+          <div className="relative flex flex-col items-center justify-center px-4 pb-1">
+            <h2 className="flex items-baseline w-full pt-2 text-3xl font-bold leading-normal text-center text-white uppercase">
+              <span className="flex-1">La Resurreccion Del Rey Caido</span>
+            </h2>
+          </div>
+        </div>
+        <div
           className="relative block w-full max-w-2xl mx-auto font-mono shadow-sm"
           style={{
             background: "rgba(40, 17, 54, 0.91)",
@@ -660,7 +718,7 @@ const EpisodiosPage = () => {
           </div>
         </div>
       </div>
-      <div className="max-w-6xl py-3 py-12 m-auto text-center">
+      <div className="max-w-6xl py-12 m-auto text-center">
         <Link
           to={`/notanenvivo/`}
           className="px-4 py-3 my-3 font-sans text-lg text-white transition duration-150 ease-in-out bg-red-700 hover:text-red-100 hover:bg-red-800"
--- src/pages/index.js
+++ src/pages/index.js
@@ -140,12 +140,12 @@ const IndexPage = () => {
               title="La deuda"
               frameBorder="no"
               allow="autoplay"
-              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1097231977&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1105144891&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
             ></iframe>
           </div>
           <div className="relative flex flex-col items-center justify-center px-4 pb-1">
             <h2 className="flex items-baseline w-full pt-2 text-4xl font-bold leading-normal text-center text-white uppercase">
-              <span className="flex-1">¿Quieres Ser Marcio Rizo?</span>
+              <span className="flex-1"> Historias De Gente Común (como Vos, Como Yo, Como Aquel)</span>
             </h2>
           </div>
         </div>
