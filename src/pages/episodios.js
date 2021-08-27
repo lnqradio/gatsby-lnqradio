874b952497882ad1c39eb1441eb711af2872a270
stn <smrcouto88@gmail.com>
2021-08-27 15:28:50 -0300

    le que se dice la lleca

--- src/pages/episodios.js
+++ src/pages/episodios.js
@@ -79,6 +79,34 @@ const EpisodiosPage = () => {
         />
       </div>
       <div className="grid max-w-6xl grid-cols-1 gap-6 mx-auto md:grid-cols-2">
+      <div
+          className="relative block w-full max-w-2xl mx-auto font-mono shadow-sm"
+          style={{
+            background: "rgba(40, 17, 54, 0.91)",
+          }}
+        >
+          <div
+            className="flex items-center justify-between listen"
+            style={{
+              opacity: 0.9,
+            }}
+          >
+            <iframe
+              width="100%"
+              height="350"
+              scrolling="no"
+              title="La deuda"
+              frameBorder="no"
+              allow="autoplay"
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1113834775&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
+            ></iframe>
+          </div>
+          <div className="relative flex flex-col items-center justify-center px-4 pb-1">
+            <h2 className="flex items-baseline w-full pt-2 text-3xl font-bold leading-normal text-center text-white uppercase">
+              <span className="flex-1">Lo Que Se Dice La Lleca</span>
+            </h2>
+          </div>
+        </div>
         <div
           className="relative block w-full max-w-2xl mx-auto font-mono shadow-sm"
           style={{
@@ -103,9 +131,7 @@ const EpisodiosPage = () => {
           </div>
           <div className="relative flex flex-col items-center justify-center px-4 pb-1">
             <h2 className="flex items-baseline w-full pt-2 text-3xl font-bold leading-normal text-center text-white uppercase">
-              <span className="flex-1">
-                El Poema 28
-              </span>
+              <span className="flex-1">El Poema 28</span>
             </h2>
           </div>
         </div>
