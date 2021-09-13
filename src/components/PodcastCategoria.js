68c723349c7412ae3c61066485c6563f83dcfda7
stn <smrcouto88@gmail.com>
2021-09-13 19:12:32 -0300

    astrologia

--- src/components/PodcastCategoria.js
+++ src/components/PodcastCategoria.js
@@ -1,8 +1,14 @@
 import { Link } from "gatsby"
 import React from "react"
-import { GiSpellBook, GiFlame, GiBookCover, GiRomanToga } from "react-icons/gi"
+import {
+  GiSpellBook,
+  GiFlame,
+  GiBookCover,
+  GiRomanToga,
+  GiRingedPlanet,
+} from "react-icons/gi"
 import { MdPersonPin, MdLocalMovies } from "react-icons/md"
-import { IoMdMicrophone, IoMdMusicalNotes } from "react-icons/io"
+import { IoMdMicrophone, IoMdMusicalNotes, IoPlanet } from "react-icons/io"
 
 const PodcastCategorias = () => (
   <>
@@ -12,10 +18,8 @@ const PodcastCategorias = () => (
     </Link>
     <Link to={`/podcasts/cine`} className="bg-gray-800 hover:text-white">
       <MdLocalMovies />
-
       <span>cine</span>
     </Link>
-
     <Link
       to={`/podcasts/entrevistas/`}
       className="bg-gray-800 hover:text-white"
@@ -25,27 +29,22 @@ const PodcastCategorias = () => (
     </Link>
     <Link to={`/podcasts/perfiles`} className="bg-gray-800 hover:text-white">
       <MdPersonPin />
-
       <span>perfiles</span>
     </Link>
     <Link to={`/podcasts/historias`} className="bg-gray-800 hover:text-white">
       <GiSpellBook />
-
       <span>historias</span>
     </Link>
-    <Link to={`/podcasts/previas`} className="bg-gray-800 hover:text-white">
-      <GiFlame />
-
-      <span>previas</span>
+    <Link to={`/podcasts/astrologia`} className="bg-gray-800 hover:text-white">
+      <GiRingedPlanet />
+      <span>Astrología</span>
     </Link>
     <Link to={`/podcasts/politica`} className="bg-gray-800 hover:text-white">
       <GiRomanToga />
-
       <span>política</span>
     </Link>
     <Link to={`/podcasts/literatura`} className="bg-gray-800 hover:text-white">
       <GiBookCover />
-
       <span>Literatura</span>
     </Link>
   </>
--- src/pages/podcasts/astrologia.js
+++ src/pages/podcasts/astrologia.js
@@ -3,17 +3,19 @@ import { Link, useStaticQuery, graphql } from "gatsby"
 import { kebabCase } from "lodash"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+import PodcastHero from "../../components/PodcastHero"
+import {
+  GiRingedPlanet
+} from "react-icons/gi"
 
 const AstrologiaPage = () => {
   return (
     <Layout>
       <SEO title="Astrología" />
-      <div className="relative z-10 flex flex-col items-center justify-center px-6 py-32 pt-48 m-0 mb-6 bg-gray-900 md:sticky md:top-0 hero">
-        <h1 className="font-mono text-6xl text-center text-white ">
-          Astrología
-        </h1>
-        <h2>Cada Mente Un Planeta</h2>
-      </div>
+      <PodcastHero
+        heading="Cada Mente Un Planeta"
+        icon={<GiRingedPlanet className="text-6xl" />}
+      />
       <div className="grid max-w-6xl grid-cols-1 gap-6 pb-12 mx-auto">
         <div
           className="relative block w-full max-w-5xl mx-auto font-mono shadow-sm"
@@ -29,7 +31,7 @@ const AstrologiaPage = () => {
           >
             <iframe
               width="100%"
-              height="250"
+              height="150"
               scrolling="no"
               title="La deuda"
               frameBorder="no"
