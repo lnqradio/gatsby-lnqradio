57a7307840e1c9e30333b0f2153066470041abd2
stn <smrcouto88@gmail.com>
2021-10-21 13:05:19 -0300

    luna llena en aries

--- src/pages/podcasts/astrologia.js
+++ src/pages/podcasts/astrologia.js
@@ -43,6 +43,36 @@ const AstrologiaPage = () => {
               title="2 - Los Retrogrados (una Caricia A Mercurio)"
               frameBorder="no"
               allow="autoplay"
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1145458177&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
+            ></iframe>
+          </div>
+          <div className="relative flex flex-col items-center justify-center px-4 pb-1">
+            <h2 className="flex items-baseline w-full pt-2 text-3xl font-bold leading-normal text-left text-white uppercase">
+              <span className="flex-1">
+                Capitulo 5 - Ciclo Lunar (Luna llena en Aries)
+              </span>
+            </h2>
+          </div>
+        </div>
+        <div
+          className="relative block w-full max-w-5xl mx-auto font-mono shadow-sm"
+          style={{
+            background: "rgba(40, 17, 54, 0.91)",
+          }}
+        >
+          <div
+            className="flex items-center justify-between listen"
+            style={{
+              opacity: 0.9,
+            }}
+          >
+            <iframe
+              width="100%"
+              height="250"
+              scrolling="no"
+              title="2 - Los Retrogrados (una Caricia A Mercurio)"
+              frameBorder="no"
+              allow="autoplay"
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1134531235&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
             ></iframe>
           </div>
