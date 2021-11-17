0a38d3a2429cdec347140b8bca51b3ac49f524f1
stn <smrcouto88@gmail.com>
2021-11-17 14:36:48 -0300

    de fisicos y entropia

--- src/pages/index.js
+++ src/pages/index.js
@@ -139,7 +139,7 @@ const IndexPage = () => {
           >
             <div className="relative flex flex-col items-center justify-center px-4 pb-1">
               <h2 className="flex items-baseline w-full pt-2 text-4xl font-bold leading-normal text-center text-white uppercase">
-                <span className="flex-1">Episodio 502</span>
+                <span className="flex-1">Episodio 503</span>
               </h2>
             </div>
             <div
@@ -155,12 +155,12 @@ const IndexPage = () => {
                 title="La deuda"
                 frameBorder="no"
                 allow="autoplay"
-                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1151550085&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
+                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1161932545&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
               ></iframe>
             </div>
             <div className="relative flex flex-col items-center justify-center px-4 pb-1">
               <h2 className="flex items-baseline w-full pt-2 text-4xl font-bold leading-normal text-center text-white uppercase">
-                <span className="flex-1">La Bibloteca De Papel</span>
+                <span className="flex-1">De Físicos Y Entropía</span>
               </h2>
             </div>
           </div>
