130ffb9659bc1be02205cfe862b8792dbeabc3ad
stn <smrcouto88@gmail.com>
2021-12-20 16:26:27 -0300

    sagitario y el brillo del pop

--- src/pages/podcasts/astrologia.js
+++ src/pages/podcasts/astrologia.js
@@ -24,6 +24,34 @@ const AstrologiaPage = () => {
         </a>
       </div>
       <div className="grid max-w-6xl grid-cols-1 gap-6 pb-12 mx-auto lg:grid-cols-2">
+      <div
+          className="relative block w-full max-w-5xl mx-auto font-mono shadow-sm"
+          style={{
+            background: "rgba(40, 17, 54, 0.91)",
+          }}
+        >
+          <div
+            className="flex items-center justify-between listen"
+            style={{
+              opacity: 0.9,
+            }}
+          >
+            <iframe
+              width="100%"
+              height="250"
+              scrolling="no"
+              title="2 - Los Retrogrados (una Caricia A Mercurio)"
+              frameBorder="no"
+              allow="autoplay"
+              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1179438829&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=true"
+            ></iframe>
+          </div>
+          <div className="relative flex flex-col items-center justify-center px-4 pb-1">
+            <h2 className="flex items-baseline w-full pt-2 text-3xl font-bold leading-normal text-left text-white uppercase">
+              <span className="flex-1">Capitulo 8 - Sagitario y el brillo del Pop</span>
+            </h2>
+          </div>
+        </div>
         <div
           className="relative block w-full max-w-5xl mx-auto font-mono shadow-sm"
           style={{
